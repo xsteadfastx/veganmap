e286c866280e5b5769ba19e1d58112fdf907ca26
Marvin Steadfast <xsteadfastx@gmail.com>
2013-08-08 10:49:11 +0200

    fixed attribution for the cloudmade tiles and rearanged the layout

--- veganmap.js
+++ veganmap.js
@@ -19,18 +19,28 @@ function veganmap() {
   });
 
   var tileCloudMade = L.tileLayer('http://{s}.tile.cloudmade.com/9fc04dd0af2241b38e5aeea5d93c2655/997/256/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '&copy; 2013 <a href="http://cloudmade.com">CloudMade</a> &#8211; Map data <a href="http://www.openstreetmap.org/copyright">ODbL</a> 2013 <a href="http://www.openstreetmap.org/">OpenStreetMap.org</a> contributors &#8211; <a href="http://cloudmade.com/website-terms-conditions">Terms of Use</a>&#8221;',
     maxZoom: 18
   });
 
-  var map = L.map('map', { center: [0, 0], zoom: 3, layers: [tileCloudMade], worldCopyJump: true });
+  var map = L.map('map', { center: [0, 0], zoom: 3, layers: [tileCloudMade], zoomControl: false, worldCopyJump: true });
 
-  var layers = L.control.layers({
+  var layers = {
     "OpenStreetMap": tileOSM,
     "MapQuestOpen": tileMapQuest,
     "Toner": tileToner,
     "CloudMade": tileCloudMade,
-  }).addTo(map);
+  };
+
+  var layerControl = L.control.layers(layers, null, {position: 'bottomright'});
+  
+  layerControl.addTo(map);
+
+  var zoomControl = L.control.zoom({
+                    position: 'bottomright'
+                });
+
+  map.addControl(zoomControl);
 
   var markers = new L.MarkerClusterGroup({showCoverageOnHover: false, maxClusterRadius: 32});
 
